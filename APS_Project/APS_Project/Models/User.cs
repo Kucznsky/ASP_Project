ac032c794b3350c2ef131d668f619c80fc1f7dc8
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-05-13 11:45:51 +0200

    updates_in_user

--- APS_Project/APS_Project/Models/User.cs
+++ APS_Project/APS_Project/Models/User.cs
@@ -8,15 +8,15 @@ namespace APS_Project.Models
 {
     public class User
     {
-        int UserId { get; set; }
+        public int UserId { get; set; }
         [ForeignKey("RecipeId")]
         public IEnumerable<int> ListOfFavs { get; set; }
         [ForeignKey("RecipeId")]
         public IEnumerable<int> ListOfMyRecepies {get;set;}
-        string Name { get; set; }
-        string LastName { get; set; }
-        string Email { get; set;}
-        string RegistrationDate { get; set; }
-        string Description { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set;}
+        public string RegistrationDate { get; set; }
+        public string Description { get; set; }
     }
 }
