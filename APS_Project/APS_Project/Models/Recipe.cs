b1e2bc7b648becb060960fe8a6d1acd13f3fb546
Fresh2054 <freshmultikk21@gmail.com>
2021-05-13 11:38:13 +0200

    as

--- APS_Project/APS_Project/Models/Recipe.cs
+++ APS_Project/APS_Project/Models/Recipe.cs
@@ -7,5 +7,17 @@ namespace APS_Project.Models
 {
     public class Recipe
     {
+        public int RecipeId { get; set; }
+        public string Title { get; set; }
+        public byte[] Image { get; set; }
+        public IDictionary<string, string> IngredientsWithQuantity { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<int> Upvoters { get; set; }
+        public IEnumerable<int> Downvoters { get; set; }
+        public DateTime PublicationDate { get;set; }
+        public DateTime LastEditDate { get;set; }
+        public int OwnerId { get; set; }
+        public IEnumerable<string> GaleryLinks { get; set; }
+
     }
 }
