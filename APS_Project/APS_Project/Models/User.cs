ac8a5ed3a034675aeeb7b3ea40120050c270c819
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-05-13 11:41:24 +0200

    user

--- APS_Project/APS_Project/Models/User.cs
+++ APS_Project/APS_Project/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,15 @@ namespace APS_Project.Models
 {
     public class User
     {
+        int UserId { get; set; }
+        [ForeignKey("RecipeId")]
+        public IEnumerable<int> ListOfFavs { get; set; }
+        [ForeignKey("RecipeId")]
+        public IEnumerable<int> ListOfMyRecepies {get;set;}
+        string Name { get; set; }
+        string LastName { get; set; }
+        string Email { get; set;}
+        string RegistrationDate { get; set; }
+        string Description { get; set; }
     }
 }
