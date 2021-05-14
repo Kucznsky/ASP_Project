af81e470c414a362471b59719b97c678397cfb85
Fresh2054 <freshmultikk21@gmail.com>
2021-05-14 14:52:53 +0200

    layout

--- APS_Project/wwwroot/Data/python_img_to_string.py
+++ APS_Project/wwwroot/Data/python_img_to_string.py
@@ -3,7 +3,6 @@ import base64
   
 with open("Logo.png", "rb") as image2string:
     converted_string = base64.b64encode(image2string.read())
-print(converted_string)
   
 with open('exitstring', "wb") as file:
     file.write(converted_string)
