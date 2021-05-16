65da7e3142dec0dc554e8f22f61d470a2897bbc8
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-05-16 02:19:27 +0200

    publish

---
+++ APS_Project/Pages/Publish.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APS_Project.Pages
+{
+    public class PublishModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
