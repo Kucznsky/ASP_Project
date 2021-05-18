434865773919a85d35c27d966e0a8b1cccb4788c
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-05-18 18:16:37 +0200

    Publish

--- APS_Project/Pages/Publish.cshtml.cs
+++ APS_Project/Pages/Publish.cshtml.cs
@@ -2,13 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APS_Project.Data;
+using APS_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace APS_Project.Pages
 {
     public class PublishModel : PageModel
     {
+        private readonly ILogger<PublishModel> _logger;
+        private readonly ApplicationDbContext _dbContext;
+        public Recipe Recipe { get; set; }
+        public Catergory Category { get; set; }
+        public RecipeIngredient RecipeIngredient { get; set; }
+        public PublishModel(ILogger<PublishModel> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
         public void OnGet()
         {
         }
