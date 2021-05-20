266d5516014912f6bc234cc2ab93cb0799897a76
Fresh2054 <freshmultikk21@gmail.com>
2021-05-20 09:36:31 +0200

    id

--- APS_Project/Pages/MyFavourite.cshtml.cs
+++ APS_Project/Pages/MyFavourite.cshtml.cs
@@ -2,15 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APS_Project.Data;
+using APS_Project.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace APS_Project.Pages
 {
+    [Authorize]
     public class MyFavouriteModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _dbContext;
+        public List<Recipe> Recipes { get; set; }
+        public List<UserFavourite> UserFavourites { get; set; }
+
+        public MyFavouriteModel(ApplicationDbContext dbContext) 
         {
+            _dbContext = dbContext;
+        }
+        public async Task OnGetAsync(int userId)
+        {
+            UserFavourites = await _dbContext.UserFavourites.Where(p => p.UserId == userId).ToListAsync();
+            foreach (var userFav in UserFavourites)
+            {
+                Recipes.Add(_dbContext.Recipes.Single(p => p.RecipeId == userFav.RecipeId));
+            }
         }
     }
 }
